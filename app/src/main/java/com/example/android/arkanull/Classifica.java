a02854faf9d4c69384b6bc2c46e4a2a837c15671
Piemat1 <76700948+Piemat1@users.noreply.github.com>
2021-07-27 19:28:36 +0200

    ciao

--- app/src/main/java/com/example/android/arkanull/Classifica.java
+++ app/src/main/java/com/example/android/arkanull/Classifica.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class Classifica extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

360a6905c5159bb97d3f5a01b4e3eef7666f4e87
Domepicc <74468057+Domepicc@users.noreply.github.com>
2021-07-29 17:12:21 +0200

    Rimozione modifiche firebase

--- app/src/main/java/com/example/android/arkanull/Account.java
+++ app/src/main/java/com/example/android/arkanull/Account.java
@@ -1,7 +1,8 @@
 package com.example.android.arkanull;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class Account extends AppCompatActivity {
 

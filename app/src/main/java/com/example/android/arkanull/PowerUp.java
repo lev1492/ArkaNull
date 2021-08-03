90d319c3f2ad14f9baedef7be3995780b155bc8b
Marco <marco.nardella97@gmail.com>
2021-08-03 17:54:43 +0200

    = Tradotto in inglese alcuni commenti + Aggiunto una png per l'icona del power up + Aggiunta classe Power Up è incompleta e c'è solo la funzionalità "di base", l'icona cade sul giocatore, se la raccoglie con la barra in basso la pallina non rimbalza sui mattoncini per un breve periodo di tempo Il tempo l'ho implementato come un int che viene abbassato ad ogni ciclo di update, magari ci sono metodi migliori ma quello era il più veloce.

---
+++ app/src/main/java/com/example/android/arkanull/PowerUp.java
@@ -0,0 +1,66 @@
+package com.example.android.arkanull;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+
+public class PowerUp{
+    private Bitmap pwrUp;
+    private float x;
+    private float y;
+
+    public PowerUp( float x, float y){
+        this.x = x;
+        this.y = y;
+
+
+    }
+
+    private boolean isClose(float ax, float ay, float bx, float by) {
+        bx += 12;
+        by += 11;
+        if ((Math.sqrt(Math.pow((ax + 50) - bx, 2) + Math.pow(ay - by, 2))) < 80) {
+            return true;
+        } else if ((Math.sqrt(Math.pow((ax + 100) - bx, 2) + Math.pow(ay - by, 2))) < 60) {
+            return true;
+        } else if ((Math.sqrt(Math.pow((ax + 150) - bx, 2) + Math.pow(ay - by, 2))) < 60) {
+            return true;
+        }
+        return false;
+    }
+
+    // If the ball collides with the fall, it will change direction
+    protected boolean touchPaddle(float xPaddle, float yPaddle) {
+        if (isClose(xPaddle, yPaddle, getX(), getY())) return true;
+        return false;
+    }
+
+
+    public void fall(){
+        y = y + 10;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+
+    public float getY() {
+        return y;
+    }
+
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public Bitmap getPwrUp() {
+        return pwrUp;
+    }
+}

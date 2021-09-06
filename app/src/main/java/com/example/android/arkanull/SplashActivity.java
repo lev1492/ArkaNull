37b7f7d7e57c05267d7726f9b7df9eac3b867881
Piemat1 <76700948+Piemat1@users.noreply.github.com>
2021-09-06 13:34:35 +0200

    modifica schermata splash
    modifica schermata splash

--- app/src/main/java/com/example/android/arkanull/SplashActivity.java
+++ app/src/main/java/com/example/android/arkanull/SplashActivity.java
@@ -11,16 +11,16 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();
         Intent intent = new Intent(this , LoginActivity.class);
         final Handler handler  = new Handler();
         final Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 startActivity(intent);
+                finish();
             }
         };
         handler.postDelayed(runnable , 1000);
+
     }
 }
